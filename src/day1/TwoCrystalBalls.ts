5dbed5db4e0df7ff6f8dd6b0245f5b2ccc8cc0c8
krehwell <fahrimuham001@gmail.com>
2023-05-31 21:49:54 +0700

    crystal ball and bubble sort

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,20 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let jmp = Math.floor(Math.sqrt(breaks.length));
+    let i = jmp
 
+    for (; i < breaks.length; i += jmp) {
+        if (breaks[i]) {
+            break
+        }
+    }
+
+    i -= jmp
+
+    for (let j = i; j < i + jmp; j++) {
+        if (breaks[j]) {
+            return j
+        }
+    }
+
+    return -1;
 }
